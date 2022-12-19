211d0c45def208c0f6e9861b38af9176ff2dbb89
maggie-light <mosesmargret2@gmail.com>
2022-12-19 22:42:27 +0100

    task  zero

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,10 @@ def safe_print_list(my_list=[], x=0):
     my_list = (0, 1, 2, 3)
     x = my_list
     print(x[my_list])
-    except:
+    except(IndexError)
+    except IndexError:
         print("zero")
+
 
 
 
