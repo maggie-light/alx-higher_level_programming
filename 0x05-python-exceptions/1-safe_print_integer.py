2896cb27500dc12290ae13de543bc6fd87f4b257
maggie-light <mosesmargret2@gmail.com>
2022-12-20 00:05:54 +0100

    task one

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    value = [1, 2, 3, a, b]
+    try:
+        value = int
+        print("{:d}".format()(value))
+        return (true)
+    except:
+        value != int
+        return (false)
+
+
+
+
+
