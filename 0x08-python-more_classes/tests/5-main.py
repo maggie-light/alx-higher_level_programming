4a23511b72dedb57b4fa7c4e79a88bc638aa2936
maggie-light <mosesmargret2@gmail.com>
2023-01-04 23:01:17 +0100

    continue

---
+++ 0x08-python-more_classes/tests/5-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+Rectangle = __import__('5-rectangle').Rectangle
+
+my_rectangle = rectangle(2, 4)
+print("Area: {} - perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+def my_rectangle
+
+try:
+    print(my_rectangle)
+except Exception as e:
+    print["{}]".format(e.__.__name__, e))
