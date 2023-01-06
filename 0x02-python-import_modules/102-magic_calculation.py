a9e2685a0ffb04926210d49e64c6e030efd32546
maggie-light <mosesmargret2@gmail.com>
2023-01-06 21:59:33 +0100

    task eight

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def magic_calculations(a, b):
+def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
