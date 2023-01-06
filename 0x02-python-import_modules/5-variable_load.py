4c97021f2756bf9527a240ea9733aabd7267f98b
maggie-light <mosesmargret2@gmail.com>
2023-01-06 10:28:41 +0100

    task five

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main""":
-    """print the value of variable a from variable_load_5."""
-    from variable_laod_5 import a
+    """print the value of variable a from variable_load_5.py"""
+    from variable_laod_5.py import a
 
     print(a)
